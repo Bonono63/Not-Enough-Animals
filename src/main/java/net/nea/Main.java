7b1212dbb360fb1f54b81c1b7c576efcf853131b
Bonono63 <bonoria63@gmail.com>
2020-06-02 18:12:36 -0400

    registry update

--- src/main/java/net/nea/Main.java
+++ src/main/java/net/nea/Main.java
@@ -1,9 +1,24 @@
 package net.nea;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 import net.nea.registry.NEntities;
 
 public class Main implements ModInitializer {
+
+	public static final String MOD_ID = "nea";
+
+	public static Identifier id(String name) {
+		return new Identifier(MOD_ID, name);
+	}
+
+	public static final ItemGroup GROUP = FabricItemGroupBuilder.build(id("nea"), () -> new ItemStack(Items.BLUE_CARPET));
+
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
--- src/main/java/net/nea/registry/NEntityTypes.java
+++ src/main/java/net/nea/registry/NEntityTypes.java
@@ -1,5 +1,19 @@
 package net.nea.registry;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.registry.Registry;
+import net.nea.Main;
+
 public class NEntityTypes {
 
+    public NEntityTypes()
+    {}
+
+    public static void init() {
+    }
+
+    private static <T extends Entity> EntityType<T> register(String name, EntityType<T> entity) {
+        return Registry.register(Registry.ENTITY_TYPE, Main.id(name), entity);
+    }
 }
